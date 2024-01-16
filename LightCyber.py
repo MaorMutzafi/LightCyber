1b456968ce883834e8a51bca5de87651bdafa14d
MaorMutzafi <maormutzafi@gmail.com>
2024-01-16 20:23:09 +0200

    Divide the algo into two section so that it will return xy and 2 type of scores for every frame in the batch

--- LightCyber.py
+++ LightCyber.py
@@ -61,8 +61,8 @@ def LightCyberAlg(input_tensor, sigma, win_sz_std, max_corners_harris=10,
 
     # Gaussian filter preparation
     f_wid = round(3 * sigma)
-    gaussian = torch.exp(-torch.arange(-f_wid, f_wid + 1)**2/2/sigma**2)/(2*torch.pi*sigma**2)**.5
-    G1_1D = torch.tensor(gaussian, dtype=torch.float32).to(device)
+    G1_1D = (torch.exp(-torch.arange(-f_wid, f_wid + 1)**2/2/sigma**2)/(2*torch.pi*sigma**2)**.5).to(device)
+    #G1_1D = torch.tensor(gaussian, dtype=torch.float32).to(device)
     G1 = G1_1D.reshape(1, 1, -1, 1)
     G1_transpose = G1_1D.reshape(1, 1, 1, -1)
     G2 = torch.mul(G1, G1_transpose)
@@ -82,62 +82,54 @@ def LightCyberAlg(input_tensor, sigma, win_sz_std, max_corners_harris=10,
 
     # Non-maximal suppression and corner strength thresholding
     max_pool = F.max_pool2d(harris.unsqueeze(1), kernel_size=int(2 * radius + 1), stride=1, padding=radius)
-    corners = harris * ((harris == max_pool.squeeze(1)) & (harris > low_thresh_harris))
+    corner_strengths = harris * ((harris == max_pool.squeeze(1)) & (harris > low_thresh_harris))
 
     # Parameters for batch and image dimensions
     batch_size, height, width = input_tensor.shape
 
     # Find top max_corners_harris corners in each frame
-    corners_vals, idx = torch.topk(corners.view(corners.size(0), -1), k=max_corners_harris)
-    corners_y, corners_x = idx.div(corners.size(2), rounding_mode='floor'), idx % corners.size(2)
+    corners_vals, idx = torch.topk(corner_strengths.view(corner_strengths.size(0), -1), k=max_corners_harris)
+    corners_y, corners_x = idx.div(corner_strengths.size(2), rounding_mode='floor'), idx % corner_strengths.size(2)
 
-    if without_loops_flg:
-        # Vectorized computation of standard deviation in corner windows
-        disp_x, disp_y = torch.meshgrid(torch.arange(-win_sz_std // 2, win_sz_std // 2 + 1).to(device), 
-                                        torch.arange(-win_sz_std // 2, win_sz_std // 2 + 1).to(device), indexing='ij')
-        disp_x = disp_x.reshape(1, 1, -1, 1, 1)
-        disp_y = disp_y.reshape(1, 1, -1, 1, 1)
-        
-        window_x = (corners_x.view(batch_size, max_corners_harris, 1, 1, 1) + disp_x).clamp(min=0, max=width - 1)
-        window_y = (corners_y.view(batch_size, max_corners_harris, 1, 1, 1) + disp_y).clamp(min=0, max=height - 1)
-        
-        std_vals = input_tensor[torch.arange(batch_size).view(batch_size, 1, 1, 1, 1).expand_as(window_x), window_y, window_x].std(dim=[2, 3, 4])
-        corner_std_vals = torch.zeros_like(corners)  # Initialize corner std values
-        corner_std_vals[
-            torch.arange(batch_size).view(-1, 1, 1),
-            corners_y[:, :max_corners_harris].unsqueeze(2),
-            corners_x[:, :max_corners_harris].unsqueeze(2)
-        ] = std_vals[:, :max_corners_harris].unsqueeze(2)
-        
-        corner_strengths = corners.mean(dim=0)
-        corner_std_vals = corner_std_vals.mean(dim=0)
-    else:
-        # Loop-based computation of corner strengths and standard deviation values
-        corner_strengths = torch.zeros_like(input_tensor[0])
-        corner_std_vals = torch.zeros_like(input_tensor[0])
-        
-        for b in range(batch_size):
-            for i in range(max_corners_harris):
-                corner_strengths[corners_y[b, i], corners_x[b, i]] += corners_vals[b, i]
-                window = input_tensor[b, 
-                                      max(corners_y[b, i] - win_sz_std // 2, 0):min(corners_y[b, i] + win_sz_std // 2 + 1, height),
-                                      max(corners_x[b, i] - win_sz_std // 2, 0):min(corners_x[b, i] + win_sz_std // 2 + 1, width)]
-                corner_std_vals[corners_y[b, i], corners_x[b, i]] += window.std()
-
-        # Average over the entire batch
-        corner_strengths /= batch_size
-        corner_std_vals /= batch_size
+    # Vectorized computation of standard deviation in corner windows
+    disp_x, disp_y = torch.meshgrid(torch.arange(-win_sz_std // 2, win_sz_std // 2 + 1).to(device), 
+                                    torch.arange(-win_sz_std // 2, win_sz_std // 2 + 1).to(device), indexing='ij')
+    disp_x = disp_x.reshape(1, 1, -1, 1, 1)
+    disp_y = disp_y.reshape(1, 1, -1, 1, 1)
     
+    window_x = (corners_x.view(batch_size, max_corners_harris, 1, 1, 1) + disp_x).clamp(min=0, max=width - 1)
+    window_y = (corners_y.view(batch_size, max_corners_harris, 1, 1, 1) + disp_y).clamp(min=0, max=height - 1)
+    
+    std_vals = input_tensor[torch.arange(batch_size).view(batch_size, 1, 1, 1, 1).expand_as(window_x), window_y, window_x].std(dim=[2, 3, 4])
+
+    return corners_y, corners_x, corners_vals, std_vals, corner_strengths
+
+def alg_final_res(corner_strengths, std_vals, corners_x, corners_y, max_corners_harris, radius, threshold, sigma, device):
+    # Gaussian filter preparation
+    f_wid = round(3 * sigma)
+    G1_1D = (torch.exp(-torch.arange(-f_wid, f_wid + 1)**2/2/sigma**2)/(2*torch.pi*sigma**2)**.5).to(device)
+    #G1_1D = torch.tensor(gaussian, dtype=torch.float32).to(device)
+    G1 = G1_1D.reshape(1, 1, -1, 1)
+    G1_transpose = G1_1D.reshape(1, 1, 1, -1)
+
+    batch_size = corner_strengths.shape[0]
+    corner_std_vals = torch.zeros_like(corner_strengths)  # Initialize corner std values
+    corner_std_vals[
+        torch.arange(batch_size).view(-1, 1, 1),
+        corners_y[:, :max_corners_harris].unsqueeze(2),
+        corners_x[:, :max_corners_harris].unsqueeze(2)
+    ] = std_vals[:, :max_corners_harris].unsqueeze(2)
+        
     # Apply threshold to corner_std_vals and find coordinates
     use_corner_std_flg = True
     if use_corner_std_flg:
-        std_values_g = F.conv2d(F.conv2d(corner_std_vals.unsqueeze(0).unsqueeze(0), G1, padding='same'), G1_transpose, padding='same')
+        std_values_g = F.conv2d(F.conv2d(corner_std_vals.mean(dim=0).unsqueeze(0).unsqueeze(0), G1, padding='same'), G1_transpose, padding='same')
         max_pool_std = F.max_pool2d(std_values_g, kernel_size=int(2 * radius + 1), stride=1, padding=radius).squeeze(0).squeeze(0)
-        threshold_mask = ((corner_std_vals > threshold) & (std_values_g == max_pool_std) & (corner_std_vals != 0))
+        threshold_mask = ((corner_std_vals.mean(dim=0) > threshold) & (std_values_g == max_pool_std) & (corner_std_vals.mean(dim=0) != 0))
     else:
-        con_values_g = F.conv2d(F.conv2d(corner_strengths.unsqueeze(0).unsqueeze(0), G1, padding='same'), G1_transpose, padding='same')
+        con_values_g = F.conv2d(F.conv2d(corner_strengths.mean(dim=0).unsqueeze(0).unsqueeze(0), G1, padding='same'), G1_transpose, padding='same')
         max_pool_std = F.max_pool2d(con_values_g, kernel_size=int(2 * radius + 1), stride=1, padding=radius).squeeze(0).squeeze(0)
-        threshold_mask = ((corner_std_vals > threshold) & (con_values_g == max_pool_std) & (corner_std_vals != 0))
+        threshold_mask = ((corner_std_vals.mean(dim=0) > threshold) & (con_values_g == max_pool_std) & (corner_std_vals.mean(dim=0) != 0))
     xy_threshold = threshold_mask.squeeze(0).squeeze(0).nonzero()
     
     return corner_strengths, corner_std_vals, xy_threshold
@@ -175,10 +167,10 @@ def depack_raw_data_torch(dat_all, width, height, device='cuda', n_fr = None):
     comb = comb.permute(*reversed(range(len(comb.shape)))).reshape(*reversed((width , height, n_fr))).permute(*reversed(range(len((width , height, n_fr))))).permute(2, 1, 0)
     return comb
 
-def LoadGainOffset_torch(GainOffsetDirPath, width, height, device):
+def LoadGainOffset_torch(gain_path,offset_path, width, height, device):
     # Read Gain and Offset using NumPy, then convert to PyTorch tensors
-    Offset = torch.from_numpy(np.fromfile(GainOffsetDirPath + 'Offset.raw', dtype=np.uint16).reshape(width, height).T.astype(np.int16)).to(device)
-    Gain = torch.from_numpy(np.fromfile(GainOffsetDirPath + 'Gain.raw', dtype=np.float64).reshape(width, height).T.astype(np.float32)).to(device)
+    Offset = torch.from_numpy(np.fromfile(offset_path, dtype=np.uint16).reshape(width, height).T.astype(np.int16)).to(device)
+    Gain = torch.from_numpy(np.fromfile(gain_path, dtype=np.float64).reshape(width, height).T.astype(np.float32)).to(device)
     
     return Gain, Offset
 
@@ -283,8 +275,8 @@ def visualize_results(corner_strengths, std_values, xy_threshold, input_image):
 
     plt.show()
 
-def main():
-    # camera params
+def set_params():
+        # camera params
     width = 640
     height = 268
     # alg params
@@ -295,9 +287,14 @@ def main():
     radius = 3
     threshold = 2
     use_cuda = True # use cuda if avaible
-
+    # files path    
     file_path = 'C:/Users/User/Documents/Mafaat_new_Topics/LightCyber/Exp/Kfir_11June2023/Data/OS_12062023024132_70m_4mW/Vid.raw'
-    GainOffsetDirPath = 'C:/Users/User/Documents/Mafaat_new_Topics/LightCyber/Exp/Kfir_11June2023/Data/GainOffset/'
+    gain_path = 'C:/Users/User/Documents/Mafaat_new_Topics/LightCyber/Exp/Kfir_11June2023/Data/GainOffset/Gain.raw'
+    offset_path = 'C:/Users/User/Documents/Mafaat_new_Topics/LightCyber/Exp/Kfir_11June2023/Data/GainOffset/Offset.raw'
+
+    return width, height, sigma, win_sz_std, max_corners_harris, low_thresh_harris, radius, threshold, use_cuda, file_path, gain_path, offset_path
+
+def pre_process(file_path, width, height, use_cuda, gain_path,offset_path):
 
     if use_cuda and torch.cuda.is_available():
         device = torch.device("cuda")
@@ -309,9 +306,15 @@ def main():
     dat_all_np = load_numpy_dat(file_path, width, height)
 
     # pre-process
-    Gain, Offset = LoadGainOffset_torch(GainOffsetDirPath, width, height, device=device.type)
+    Gain, Offset = LoadGainOffset_torch(gain_path,offset_path, width, height, device=device.type)
     neighborhoods, bad_pixels_flat = find_bad_pxl_torch(Gain, width, height, device)
+    return Gain, Offset, dat_all_np, neighborhoods, bad_pixels_flat, device
 
+def main():
+    # set the algo parameters
+    width, height, sigma, win_sz_std, max_corners_harris, low_thresh_harris, radius, threshold, use_cuda, file_path, gain_path, offset_path = set_params()
+    # pre-processing, loading gain/offset matrices and bad pixel loc
+    Gain, Offset, dat_all_np, neighborhoods, bad_pixels_flat, device = pre_process(file_path, width, height, use_cuda, gain_path,offset_path)
 
     start_time = time.time()
 
@@ -320,15 +323,21 @@ def main():
     dat_nuc_tr = AppGainOffset_torch(dat_all_tr, Gain, Offset, device)
     Frs_corrected = bad_pxl_corr_torch(dat_nuc_tr, neighborhoods, bad_pixels_flat)
 
-    # run the alg.
-    corner_strengths, std_values, xy_threshold = LightCyberAlg(Frs_corrected, sigma = sigma, win_sz_std = win_sz_std, max_corners_harris = max_corners_harris, 
+    # run the alg. in Efcom format
+    corners_y, corners_x, corners_vals, std_vals, corner_strengths = LightCyberAlg(Frs_corrected, sigma = sigma, win_sz_std = win_sz_std, max_corners_harris = max_corners_harris, 
         low_thresh_harris = low_thresh_harris, radius = radius, threshold = threshold, device = device, 
         use_G1=True, without_loops_flg=True)
     # corner_strengths and std_values are your outputs.
     # xy_threshold is the coordinates where std_values > threshold after max pooling.
     print(time.time() - start_time)
+    print(corners_x.shape)
+    print(corners_y.shape)
+    print(corners_vals.shape)
+    print(std_vals.shape)
+    # get final results
+    corner_strengths, corner_std_vals, xy_threshold = alg_final_res(corner_strengths, std_vals, corners_x, corners_y, max_corners_harris, radius, threshold, sigma, device)
     # Show the results
-    visualize_results(corner_strengths, std_values, xy_threshold, Frs_corrected[0])
+    visualize_results(corner_strengths.mean(dim = 0), corner_std_vals.mean(dim = 0), xy_threshold, Frs_corrected[0])
 
 
 if __name__ == "__main__":
